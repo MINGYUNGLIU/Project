4e0e433593094e018f31417fa6529fc5b85d494e
MINGYUNGLIU <156177439+MINGYUNGLIU@users.noreply.github.com>
2024-02-18 19:07:53 -0800

    Update Message_encryptor.py

--- Project/Message Encryptor/Message_encryptor.py
+++ Project/Message Encryptor/Message_encryptor.py
@@ -1,8 +1,8 @@
 """
-1.Request user entering message that needed to encrypt
+1.Request the user enter a message that is needed to encrypt
 2.Generate and display a new key.
-3.Display encrypted message.The encrypted string message should be converted to bytes .
-4.Display decrypted message and decrypt the encrypted message.
+3.Display encrypted message. The encrypted string message should be converted to bytes.
+4.Display a decrypted message and decrypt the encrypted message.
 """
 from cryptography.fernet import Fernet
 
@@ -10,12 +10,12 @@ from cryptography.fernet import Fernet
 new_key=Fernet.generate_key()
 cipher_suite=Fernet(new_key)
 
-#First encode the message from data type string to bytes than encrypt the message. 
+#First encode the message from data type string to bytes then encrypt the message. 
 def encrypt_message(message):
     encrypted_message=cipher_suite.encrypt(message.encode())
     return encrypted_message
 
-#First decrypt the bytes message , than decode the message from bytes back to string. 
+#First decrypt the bytes message then decode the message from bytes back to string. 
 def decrypt_message(message):
     decrypted_message=cipher_suite.decrypt(message).decode()
     return decrypted_message
